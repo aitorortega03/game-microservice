715512834288a25f27424103a5e338e3bdc977ab
aitorortega03 <aitorortega21@gmail.com>
2024-08-25 21:01:24 +0200

    Initial commit game microservice

---
+++ src/main/java/com/example/game_microservice/GameMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameMicroserviceApplication.class, args);
+	}
+
+}
