2209b0fc396d60f65eb4dbb856aabcd2dc1ae2fd
aitorortega03 <aitorortega21@gmail.com>
2024-08-27 16:58:21 +0200

    Add logic

--- src/main/java/com/example/game_microservice/application/services/GameServiceImpl.java
+++ src/main/java/com/example/game_microservice/application/services/GameServiceImpl.java
@@ -2,19 +2,33 @@ package com.example.game_microservice.application.services;
 
 import com.example.game_microservice.application.ports.in.GameService;
 import com.example.game_microservice.domain.model.Game;
+import com.example.game_microservice.infrastructure.adapters.out.mongodb.GameRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class GameServiceImpl implements GameService {
+    private final GameRepository gameRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Override
     public Mono<Game> createGame(Game game) {
-        return null;
+        return Mono.just(game)
+                .flatMap(gameRepository::save)
+                .doOnSuccess(savedGame -> {
+                    log.info("Saved game: {}", savedGame);
+                    kafkaTemplate.send("games-topic", String.valueOf(savedGame));
+                });
     }
 
     @Override
     public Flux<Game> getAllGames() {
-        return null;
+        return gameRepository.findAll();
     }
 }
--- src/main/java/com/example/game_microservice/infrastructure/adapters/in/rest/GameController.java
+++ src/main/java/com/example/game_microservice/infrastructure/adapters/in/rest/GameController.java
@@ -1,9 +1,27 @@
 package com.example.game_microservice.infrastructure.adapters.in.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.game_microservice.application.ports.in.GameService;
+import com.example.game_microservice.domain.model.Game;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/games")
+@RequiredArgsConstructor
 public class GameController {
+    private final GameService gameService;
+
+    @PostMapping
+    public Mono<ResponseEntity<Game>> createGame(@RequestBody Game game) {
+        return gameService.createGame(game)
+                .map(ResponseEntity::ok);
+    }
+
+    @GetMapping
+    public Flux<Game> getAllGames() {
+        return gameService.getAllGames();
+    }
 }
