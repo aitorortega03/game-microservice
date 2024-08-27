6c7eb8b6118fa5ac1c03d6c509250a9cc4591954
aitorortega03 <aitorortega21@gmail.com>
2024-08-27 17:37:15 +0200

    Change to use the producer class

--- src/main/java/com/example/game_microservice/application/services/GameServiceImpl.java
+++ src/main/java/com/example/game_microservice/application/services/GameServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.game_microservice.application.services;
 
 import com.example.game_microservice.application.ports.in.GameService;
 import com.example.game_microservice.domain.model.Game;
+import com.example.game_microservice.infrastructure.adapters.out.kafka.KafkaProducer;
 import com.example.game_microservice.infrastructure.adapters.out.mongodb.GameRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class GameServiceImpl implements GameService {
     private final GameRepository gameRepository;
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaProducer kafkaProducer;
 
     @Override
     public Mono<Game> createGame(Game game) {
@@ -23,8 +24,9 @@ public class GameServiceImpl implements GameService {
                 .flatMap(gameRepository::save)
                 .doOnSuccess(savedGame -> {
                     log.info("Saved game: {}", savedGame);
-                    kafkaTemplate.send("games-topic", String.valueOf(savedGame));
-                });
+                    kafkaProducer.sendMessage("New game created: " + savedGame.getName());
+                })
+                .doOnSuccess(g -> log.info("Game sent to topic"));
     }
 
     @Override
